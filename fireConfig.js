a8ec063c8ae01162506b4976d98359d460b4b00c
hurbee <163949220+hurbee@users.noreply.github.com>
2024-05-28 15:48:00 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  aapiKey: "AIzaSyA7RQmyepC74QVTwlYBEh4Gb0SciqLEJjM",
+  authDomain: "cosm-f0c3c.firebaseapp.com",
+  databaseURL: "https://cosm-f0c3c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cosm-f0c3c",
+  storageBucket: "cosm-f0c3c.appspot.com",
+  messagingSenderId: "387634996292",
+  appId: "1:387634996292:web:c3540e918b8b3481950811"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
